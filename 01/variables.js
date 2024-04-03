c6aa2333a77a55083d453c193268bb4f546bd1b0
MD AAFTAB ALAM <maftab.alam@ssipmt.com>
2024-04-04 02:31:33 +0530

    Initial commit

---
+++ 01/variables.js
@@ -0,0 +1,46 @@
+//  in javascript we have three ways to declare variables in javaacript
+//  var, let & const
+var age = 50;
+console.log(age);
+
+var age = "dcs"
+console.log(age);
+
+//  some cool property of var 
+//  scope of var is global
+if (true){
+    var Vname = "MD"
+    // so the name variable should be inside the if statement like
+    console.log(Vname)
+    // but we know that var have global scope so we can print this 
+    // variable outside of the statement like
+}
+console.log(Vname)
+// likethat
+//  soo in var we can 
+//  1 . it is global
+//  2 . it cab be reassign 
+
+// let  it is 
+// 1. it is local 
+//  2. it can be also reassign
+
+
+if (true){
+    let a= 500
+    console.log(a)
+    //  we can reassign that as well like
+    a = 20
+    console.log(a)
+}
+
+//  cost = i guess its stands for constant
+//  we can do things like that in const
+// 1. it is local
+// 2. it  cannot be reassigned
+//  we have to initialize the value in frist step
+
+if (true){
+    const d = 30
+    console.log(d)
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Learning-javascript
+
+4/Apr/2024 
+
+    Hello there i am learing javascript in 21 days & i have also 
+    some prior knowledge of python which is helping me to learn 
+    java script
