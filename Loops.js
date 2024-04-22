c920a39e4bdcf63677ad78706dfe230609443386
MD <iammd5679@gmail.com>
2024-04-22 08:12:57 +0530

    Loop:- For loop

---
+++ Loops.js
@@ -0,0 +1,18 @@
+// Loops in javascript
+// As it name suggests that loops are those things that helps us to do repetitive
+// tasks in our code
+
+// In javascript, at basic level we have 3 types of loops they are
+// FOR - WHILE - DO WHILE
+
+// FOR LOOPS:- if we exactly know that how many time we have to use that looping
+//             than we will use for loops
+//  |------------------------| -> initializing the loop
+for (let i = 1; i <= 10; i = i + 1) 
+//      i = 1 -> start from 1
+//      i <= 10 -> upto 10
+//      i = i+1 -> add +1 every next time upto 10.
+{
+  console.log("sam "+i);
+}
+
