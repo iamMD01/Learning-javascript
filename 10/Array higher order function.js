e4f7ccabc3bc63ca03ba503c9e295c55456656b4
MD <iammd5679@gmail.com>
2024-04-25 23:59:35 +0530

    Day 10 of javascript

--- 10/Array higher order function.js
+++ 10/Array higher order function.js
@@ -1,7 +1,15 @@
 const myarray = [2.3, 2, true, false, "🤯", "Hello world"];
 console.log(myarray[0]);
-
 // This is simple array
 
 // now some cool things
 myarray.forEach((val) => console.log(val +" d"));
+
+
+myarray.map((val) => console.log(val))
+// .map return a new array each time  
+
+const numbers = [1,2,3,4,5,6,7]
+numbers.forEach((val) => console.log(val*2))
+// but in .map
+numbers.map((val) => console.log(val*2)) 
