83ac2814931fc3e948b430a6aaa295fa8d189516
MD <iammd5679@gmail.com>
2024-05-01 21:14:58 +0530

    Day 25 of javascript

--- 25/IFFE.js
+++ 25/IFFE.js
@@ -1,5 +1,12 @@
 // IFFE - Immediately invoked function expression
 (function add(a, b) {
   console.log(a + b);
-})(3, 6);
+})(3, 7);
 
+// modern implimentation in js
+(() => console.log("this is modern method "))();
+
+//
+const value = (() => 100)();
+console.log(value);
+// see mdn docs and  video 27
